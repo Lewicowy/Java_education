d434fb177d17f0e1b6f2ee44fd46de0d82c8f8cf
Danil <danil.gurev.98@list.ru>
2022-12-16 00:39:05 +0500

    feature Lab 2

--- src/Lab4/Example1.java
+++ src/Lab4/Example1.java
@@ -2,22 +2,23 @@ package Lab4;
 
 public class Example1 {
     public static void main(String[] args) {
+        byte i, j, k, figure = 10;
 
-        int l = 0, i, j, k=0, figure = 10;
-        for (i = -14; i < figure; i++) {
-            System.out.print("*");
-        }
-        System.out.print("\n");
         for (i = -1; i < figure; i++) {
-            l = l + 1;
-            System.out.print("+");
-            for (j = -12; j < figure; j++) {
-                System.out.print(" ");
-                k = k + 1;
+            if ((i == -1) || (i == figure)) {
+                for (j = -15; j < figure; j++) {
+                    System.out.print("*");
+                }
+                System.out.print("\n");
+            } else {
+                System.out.print("+");
+                for (k = -13; k < figure; k++) {
+                    System.out.print(" ");
+                }
+                System.out.println("+");
             }
-            System.out.println("+");
         }
-        for (i = -14; i < figure; i++) {
+        for (j = -15; j < figure; j++) {
             System.out.print("*");
         }
     }
