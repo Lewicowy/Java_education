a9c07af14847a91ae897a2958a8faf3c55341139
Danil <danil.gurev.98@list.ru>
2022-12-15 23:39:54 +0500

    feature Lab 2

--- src/Lab4/Example1.java
+++ src/Lab4/Example1.java
@@ -1,4 +1,24 @@
 package Lab4;
 
 public class Example1 {
+    public static void main(String[] args) {
+
+        int l = 0, i, j, k=0, figure = 10;
+        for (i = -14; i < figure; i++) {
+            System.out.print("*");
+        }
+        System.out.print("\n");
+        for (i = -1; i < figure; i++) {
+            l = l + 1;
+            System.out.print("+");
+            for (j = -12; j < figure; j++) {
+                System.out.print(" ");
+                k = k + 1;
+            }
+            System.out.println("+");
+        }
+        for (i = -14; i < figure; i++) {
+            System.out.print("*");
+        }
+    }
 }
