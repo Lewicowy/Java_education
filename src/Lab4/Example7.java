c120dfb3dead90f0e66b40c4fa479d84337ac1e4
Danil <danil.gurev.98@list.ru>
2022-12-26 00:08:33 +0500

    feature Lab 2

--- src/Lab4/Example7.java
+++ src/Lab4/Example7.java
@@ -6,26 +6,66 @@ public class Example7 {
     public static void main(String[] args) {
         Scanner jo = new Scanner(System.in);
 
-        int i, j, k, k1,l,l1;
+        int i, j, k, k1;
         System.out.print("Введите кол-во строк массива: ");
         k = jo.nextInt();
         System.out.print("Введите кол-во столбцов массива: ");
         k1 = jo.nextInt();
         int[][] arr = new int[k][k1];
-        l = 0; l1 = k1-1;
-        /*Напишите программу, в которой создается двумерный числовой массив
-и этот массив заполняется «змейкой»: сначала первая строка (слева направо),
-затем последний столбец (снизу вверх), вторая строка (слева направо) и так
-далее*/
-        int m = 0;
-        for (i = 0; i < k; i++) {
-            if (i == 0) {
-                for (j = 0; j < k1; j++) {
-                arr[i][j] = m;
+
+        int m = 1;
+        for (i = 0; i < k1; i++) {
+            arr[0][i] = m;
+            m++;
+        }
+        for (j = 1; j < k; j++) {
+            arr[j][k1-1] = m;
+            m++;
+        }
+        for (i = k1-2; i >= 0; i--) {
+            arr[k-1][i] = m;
+            m++;
+        }
+        for (j = k-2; j > 0; j--) {
+            arr[j][0] = m;
+            m++;
+        }
+        int c = 1, v = 1, n = m;
+        while (n < k * k1) {
+            while (arr[c][v + 1] == 0) {
+                arr[c][v] = m;
                 m++;
-                System.out.print(arr[i][j] + " ");
+                v++;
+            }
+            while (arr[c + 1][v] == 0) {
+                arr[c][v] = m;
+                m++;
+                c++;
+            }
+            while (arr[c][v - 1] == 0) {
+                arr[c][v] = m;
+                m++;
+                v--;
+            }
+            while (arr[c - 1][v] == 0) {
+                arr[c][v] = m;
+                m++;
+                c--;
+            }
+            n++;
+        }
+        for (i = 0; i < k; i++) {
+            for (j = 0; j < k1; j++) {
+                if (arr[i][j] == 0) {
+                    arr[i][j] = m;
                 }
             }
+        }
+        for (i = 0; i < k; i++) {
+            for (j = 0; j < k1; j++) {
+                System.out.print(arr[i][j] + "  ");
+            }
+            System.out.print("\n");
         }
     }
 }
