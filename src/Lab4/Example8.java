13e203c3f20fffd87be1a82b3eb77be78359b0b7
Danil <danil.gurev.98@list.ru>
2022-12-27 00:04:50 +0500

    feature Lab 2

--- src/Lab4/Example8.java
+++ src/Lab4/Example8.java
@@ -1,20 +1,36 @@
 package Lab4;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Example8 {
     public static void main(String[] args) {
         Scanner jo = new Scanner(System.in);
-        /*Напишите программу «Шифр Цезаря», которая зашифровывает
-введенный текст. Используете кодовую таблицу символов. При запуске
-программы в консоль необходимо вывести сообщение: «Введите текст для
-шифрования», после ввода текста, появляется сообщение: «Введите ключ».
-После того как введены все данные, необходимо вывести преобразованную
-строку с сообщением «Текст после преобразования : ». Далее необходимо
-задать вопрос пользователю: «Выполнить обратное преобразование? (y/n)»,
-если пользователь вводит «y», тогда выполнить обратное преобразование.
-Если пользователь вводит «n», того программа выводит сообщение «До
-свидания!». Если пользователь вводит что-то другое, отличное от «y» или «n»,
-то программа ему выводит сообщение: «Введите корректный ответ»*/
+        String text, choice;
+        int key;
+
+        System.out.print("Введите текст для шифрования: ");
+        text = jo.nextLine();
+        System.out.print("Введите ключ: ");
+        key = jo.nextInt();
+
+        char[] chars = text.toCharArray();
+        int[] ints = new int[text.length()];
+
+        System.out.print("Текст до преобразования: " + Arrays.toString(chars));
+        for (int i = 0; i < chars.length; i++) {
+            ints[i] = chars[i] + key;
+            chars[i] = (char)ints[i];
+        }
+        System.out.print("\nТекст после преобразования: " + Arrays.toString(chars));
+
+        System.out.print("\nВыполнить обратное преобразование? (y/n): ");
+        choice = jo.nextLine();
+
+        for(int i = 0; i < chars.length; i++) {
+            ints[i] = chars[i] - key;
+            chars[i] = (char)ints[i];
+        }
+        System.out.print("\nТекст до преобразования: " + Arrays.toString(chars));
     }
 }
